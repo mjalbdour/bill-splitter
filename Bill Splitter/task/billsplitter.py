6f860f4dbfb7eaaa091291f0e35848a4d2825942
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-23 08:13:44 +0300

    Stage 3/4: The Lucky One

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,8 +1,11 @@
+import random
 
 MSG_NUMBER_OF_PEOPLE = "Enter the number of friends joining (including you):"
 MSG_ZERO_JOINING = "No one is joining for the party"
 MSG_NAMES = "Enter the name of every friend (including you), each on a new line:"
 MSG_TOTAL_BILL = "Enter the total bill value:"
+MSG_LUCKY = 'Do you want to use the "Who is lucky?" feature? Write Yes/No:'
+MSG_NO_LUCKY = "No one is going to be lucky"
 
 print(MSG_NUMBER_OF_PEOPLE)
 num_of_people = input()
@@ -21,4 +24,11 @@ else:
     person_bill = round(total_bill / num_of_people, 2)
     for k in people_bills:
         people_bills[k] = person_bill
-    print(people_bills)
+    # print(people_bills)
+    print(MSG_LUCKY)
+    lucky = input()
+    if lucky.strip().title() in "Yes":
+        lucky_one = random.choice([p for p in people_bills])
+        print(f'{lucky_one} is the lucky one!')
+    else:
+        print(MSG_NO_LUCKY)
